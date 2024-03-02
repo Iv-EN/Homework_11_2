ef48c5f4aa0c5611cda4cf6d68f93f3b8bd3e6d0
Iv-EN <engenivanov@yandex.ru>
2024-03-02 23:30:43 +0300

    Added a function titles_words

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def translate_in_capitals(data: str) -> str:
     """Возвращает строку в верхнем регистре."""
     return data.upper()
+
+
+def titles_words(data: str) -> str:
+    """Делает заглавными первые буквы каждого слова в строке."""
+    return data.title()
